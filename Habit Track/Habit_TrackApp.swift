78a2628475a94884ee24b3b77129596ccb42304c
fauzandp <fauzan27dp@gmail.com>
2023-06-14 18:59:08 +0700

    Initial Commit

---
+++ Habit Track/Habit_TrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackApp.swift
+//  Habit Track
+//
+//  Created by Fauzan Dwi Prasetyo on 14/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
