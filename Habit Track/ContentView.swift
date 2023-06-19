854850c38a54818b45c59bc6e57bdb0e7e60bc8b
fauzandp <fauzan27dp@gmail.com>
2023-06-19 17:07:51 +0700

    Milestone-Projects7-9_HWSwiftUI | Habits Track App create UI using swiftUI using List, add nav bar create Activity Model

--- Habit Track/ContentView.swift
+++ Habit Track/ContentView.swift
@@ -8,19 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var activities = [
+        Activity(activityName: "Morning Wake Up", description: "Wake up at 3 am, then salat tahajud, pray, study and salat subuh", countTime: 0),
+        Activity(activityName: "Morning Wake", description: "Wake up at 3 am, then salat tahajud, pray, study and salat subuh", countTime: 2)
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(activities, id: \.activityName) { activity in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(activity.activityName)
+                                .font(.headline)
+                            Text(activity.description)
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                                .lineLimit(1)
+                        }
+                        
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        
+                        VStack {
+                            Text("\(activity.countTime)")
+                                .font(.title3.bold())
+                                .foregroundColor(activity.countTime > 0 ? .green : .orange)
+                            Text(activity.countTime > 1 ? "Times" : "Time")
+                                .font(.caption)
+                        }
+                    }
+                    .padding()
+                    .background(.purple)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                    .listRowSeparator(.hidden)
+                    .listRowBackground(Color.gray)
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("Habits Track")
+            .background(.gray)
+            //            .preferredColorScheme(.dark)
+            .toolbar {
+                Image(systemName: "plus")
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
